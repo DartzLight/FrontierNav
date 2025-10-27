e348177ddc6f9f7e24263ced33a629c246a224a4
Dartz <spakpower@hotmail.fr>
2025-10-27 13:46:57 +0100

    Cache the frontier nav result

---
+++ src/main/java/cardinal/xenoblade/frontiernav/FrontierNavResult.java
@@ -0,0 +1,36 @@
+package cardinal.xenoblade.frontiernav;
+
+public class FrontierNavResult {
+	private final int miranium;
+	private final int revenue;
+	private final int storage;
+
+	private FrontierNavResult(int miranium, int revenue, int storage) {
+		this.miranium = miranium;
+		this.revenue = revenue;
+		this.storage = storage;
+	}
+
+	public int getMiraniumProduction() {
+		return miranium;
+	}
+
+	public int getEffectiveMiranium() {
+		return Math.min(miranium, storage);
+	}
+
+	public int getRevenue() {
+		return revenue;
+	}
+
+	public int getStorage() {
+		return storage;
+	}
+
+	public static FrontierNavResult compute(FrontierNav frontierNav) {
+		int miranium = frontierNav.getMiranium();
+		int revenue = frontierNav.getRevenue();
+		int storage = frontierNav.getStorage();
+		return new FrontierNavResult(miranium, revenue, storage);
+	}
+}
